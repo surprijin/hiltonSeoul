7b04ce4202b4690f99213acd0e2c4d975eb3c6f0
surprijin <surprijin@naver.com>
2022-06-24 14:59:22 +0900

    commit

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,21 @@
+// 로그인 컬러 변경 
+
 $("#member").click(function(){
     $(".login_content").hide();
     $(".tab_wrapper").show();
+    $(this).css("color","#fff");
+    $(this).css("background-color","#19344D");
+    $(".nonCcolor").css("background-color","#fff");
+    $(".nonCcolor").css("color","#494949");
+
   });
   $("#nonmem").click(function(){
     $(".login_content").hide();
     $(".tab_wrapper2").show();
+    $(this).css("color", "#fff");
+    $(this).css("background-color", "#19344D");
+    $(".Ccolor").css("background-color","#fff");
+    $(".Ccolor").css("color","#494949");
   });
+
+
